1f850050a6875bcb9f6fc75f2d6cea9e2abde288
gaurav-amritkar <gaurav.amritkar@gmail.com>
2020-08-16 11:07:37 +0530

    Blog in Angular - Changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyApP0euO2-4BLesLpg1HsNTs-3kAAVAEXM',
+    authDomain: 'gaurav-amritkar-blog.firebaseapp.com',
+    databaseURL: 'https://gaurav-amritkar-blog.firebaseio.com',
+    projectId: 'gaurav-amritkar-blog',
+    storageBucket: 'gaurav-amritkar-blog.appspot.com',
+    messagingSenderId: '682582051921',
+    appId: '1:682582051921:web:c45fa238e98d22b8ebfc70',
+    measurementId: 'G-92KHQEKJPE'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyApP0euO2-4BLesLpg1HsNTs-3kAAVAEXM',
+    authDomain: 'gaurav-amritkar-blog.firebaseapp.com',
+    databaseURL: 'https://gaurav-amritkar-blog.firebaseio.com',
+    projectId: 'gaurav-amritkar-blog',
+    storageBucket: 'gaurav-amritkar-blog.appspot.com',
+    messagingSenderId: '682582051921',
+    appId: '1:682582051921:web:c45fa238e98d22b8ebfc70',
+    measurementId: 'G-92KHQEKJPE'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
